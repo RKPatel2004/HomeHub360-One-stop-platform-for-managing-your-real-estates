bc8a02847a8949098d29bbcb7d78d40d72d08591
RKPatel2004 <165505922+RKPatel2004@users.noreply.github.com>
2025-01-09 23:12:08 +0530

    Add files via upload
    Project initialization

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Sample Route
+app.get('/', (req, res) => {
+  res.send('HomeHub360 API is running...');
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
