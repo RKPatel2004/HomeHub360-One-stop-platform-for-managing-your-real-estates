0a7155e1fa10e39a076f320f5040b4835fd8f2f7
RKPatel2004 <165505922+RKPatel2004@users.noreply.github.com>
2025-02-15 23:02:42 +0530

    Delete SDP_Project_Backend (1) directory

--- SDP_Project_Backend (1)/routes/profileDetailsRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const verifyToken = require("../middleware/auth");
-const { getUserProfile } = require("../controllers/profileController");
-
-// Protected route to get user profile
-router.get("/getUserProfile", verifyToken, getUserProfile);
-
-module.exports = router;
