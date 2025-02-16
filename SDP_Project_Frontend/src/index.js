06e9c8535985a835348b351831f13824fcf66f99
RKPatel2004 <rudra0405@gmail.com>
2025-02-16 10:36:42 +0530

    index.js updated successfully

--- SDP_Project_Frontend/src/index.js
+++ SDP_Project_Frontend/src/index.js
@@ -1,3 +1,24 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
+
+
+
+
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -15,3 +36,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
